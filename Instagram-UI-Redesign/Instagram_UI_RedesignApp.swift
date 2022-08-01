502a1bf70ef776ff1aa7df6e6f7a5cc76f5f28a6
danixuz <danixuz@icloud.com>
2022-08-01 17:12:01 +0300

    Initial Commit

---
+++ Instagram-UI-Redesign/Instagram_UI_RedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_UI_RedesignApp.swift
+//  Instagram-UI-Redesign
+//
+//  Created by Daniel Spalek on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_UI_RedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
